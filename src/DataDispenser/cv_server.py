f03dc2e50c8b9a49a79a2cf6da2d3b8bbb587558
unknown <samibghanem@gmail.com>
2022-02-21 23:18:51 -0500

    Server stub description

--- src/DataDispenser/cv_server.py
+++ src/DataDispenser/cv_server.py
@@ -8,4 +8,11 @@ Combines all the other modules to send data to the server that the client Twitch
     Structure status updates.
     Game state changes.
 
+First, reads the game state to check if a game has started.
+Once a game has started (not just draft), reads one frame to get minimap coordinates.
+It takes each frame from OBS after that and detects unit/structure coordinates.
+It sends coordinates to the remote server.
+
+Then, the remote server stores them on Redis for a particular streamer.
+
 """
