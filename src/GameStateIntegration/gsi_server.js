cfdb6ba1513d3bf4cc737a6a468a2711d7adfe90
unknown <samibghanem@gmail.com>
2022-02-22 16:53:07 -0500

    node team detection and more

--- src/GameStateIntegration/gsi_server.js
+++ src/GameStateIntegration/gsi_server.js
@@ -2,6 +2,8 @@ var d2gsi = require("dota2-gsi");
 var server = new d2gsi();
 const axios = require("axios").default;
 
+var global_gsi = {team: "", building: {}, players: {}}
+
 function sendGameState(gameStateObject) {
   axios
     .post("http://127.0.0.1:8080/json", {
@@ -28,18 +30,41 @@ function sendGameState(gameStateObject) {
     .catch((err) => console.error(err));
 }
 
-/* Game started/ended.
+/* Game started/ended. - check
      Heroes alive/dead.
      Friendly structure HPs.
 
      Detecting player's:
-         Position
-         Team parity
+         Position - check
+         Team parity - check
          Player numbers and colors
          Heroes in match
          Fortification cooldown (?)
+
 */
 
+
+function detectVisibilityTeamsOrPlayerOrSpectator(gsi) {
+      if (gsi.player.hasOwnProperty('team2') || gsi.player.hasOwnProperty('team3')) {
+        // Radiant (players 0-4) and Dire (players 0-5) visible
+        global_gsi.team = "all";
+      }
+
+      if (!gsi.player.hasOwnProperty('team2') && !gsi.player.hasOwnProperty('team3')) {
+        if (gsi.player.team_name === 'spectator') {
+          // Spectating a friend, no player coordinates visible
+          global_gsi.team = "spectator";
+        }
+        // Individual player (unknown number?) visible
+        if (gsi.player.team_name === 'radiant') {
+          global_gsi.team = "radiant";
+        }
+
+        if (gsi.player.team_name === 'dire') {
+          global_gsi.team = "dire";
+        }
+      }
+}
 
 server.events.on("newclient", function (client) {
 
@@ -51,29 +76,58 @@ server.events.on("newclient", function (client) {
     console.log("No Auth token");
   }
 
+  // Game start
   client.on("player:activity", function (activity) {
-    if (activity == "playing") console.log("Game started!");
+    if (activity === "playing") console.log("Connected!");
+    console.log(activity);
+    detectVisibilityTeamsOrPlayerOrSpectator(this.gamestate);
   });
 
-  client.on("", function (activity) {
-
+  // Player team determined radiant or dire
+  client.on("player:team_name", function (team_name) {
+    console.log("Team: " + team_name);
+    global_gsi.team = team_name;
   });
 
-  client.on("hero:level", function (level) {
-    console.log(level);
-    console.log("Now level " + level);
-  });
-
-  client.on("abilities:ability0:can_cast", function (can_cast) {
-    if (can_cast) console.log("Ability0 off cooldown!");
+  // Game end- "none" - not ended. Otherwise radiant or dire
+  client.on("map:win_team", function (win_team) {
+    console.log("Game won by team: " + win_team);
   });
 
   client.on("hero:xpos", function (xpos) {
-    console.log("Hero x position: " + xpos)
+    console.log("Hero x position: " + xpos);
   })
 
   client.on("hero:ypos", function (ypos) {
     console.log("Hero y position: " + ypos)
+  })
+  // TODO
+  client.on("map:game_time", function (game_time) {
+    // Polls game state for building data every 3 seconds.
+    if (game_time % 3 === 0) {
+      if (global_gsi.team === "") {
+        detectVisibilityTeamsOrPlayerOrSpectator(this.gamestate);
+      }
+
+      if (global_gsi.team === "all") {
+
+      }
+
+      if (global_gsi.team === "spectators") {
+
+      }
+
+      if (global_gsi.team === "radiant") {
+
+      }
+
+      if (global_gsi.team === " dire") {
+
+      }
+
+      console.log("Building: " + this.gamestate.buildings[global_gsi.team])
+    }
+
   })
 
 });
