3d9e237d30ce9627b4bbd304b6546a91941b33b7
unknown <samibghanem@gmail.com>
2022-02-22 14:31:45 -0500

    node server works, doesnt send topython server yet. numbers about ingame coordinates

--- src/GameStateIntegration/gsi.py
+++ src/GameStateIntegration/gsi.py
@@ -1,77 +1,14 @@
-import subprocess
-from aiohttp import web
-from src.Models.location import Location
+import dota2gsi
 
-"""
-Combines all the other modules to send data to the server that the client Twitch extension will interface with:
-
-    All 10 hero coordinates.
-    Tower status updates.
-    Outpost status updates.
-    Structure status updates.
-    Game state changes.
- 
-It starts the node GSI server via a non-blocking subprocess call, then repeatedly polls GSI Server for:
-
-    Game started/ended.
-    Heroes alive/dead.
-    Friendly structure HPs.
-
-    Detecting player's:
-        Position
-        Team parity
-        Player numbers and colors
-        Heroes in match
-        Fortification cooldown (?)
+server = dota2gsi.Server(ip='0.0.0.0', port=8080)
 
 
-Uses https://github.com/xzion/dota2-gsi.
-
-First, reads the game state to check if a game has started. This is done via a node GSI server that is run as a
-non-blocking subprocess.
-
-Once a game has started (not just draft), reads one frame to get minimap coordinates.
-It takes each frame from OBS after that and detects unit/structure coordinates.
-It sends coordinates to the remote server.
-
-Then, the remote server stores them on Redis for a particular streamer.
-"""
+def handle_state_change(state):
+    print(state)
 
 
-def start_node_gsi_server():
-    # Popen is non-blocking.
-    subprocess.Popen(['node', '.\\gsi_server.js'])
-    return
+server.on_update(handle_state_change)
 
-
-# Start GSI server
-routes = web.RouteTableDef()
-game_started = False
-player_coordinates: Location = Location(0, 0)
-start_node_gsi_server()
-
-
-# Defines server for which events triggered in node GSI server will send notifications to
-def process_gsi(args):
-    pass
-
-
-@routes.post('/json')
-# ?page=1&uppercase=true
-async def receive_gsi(request: web.Request) -> web.Response:
-    args = await request.json()
-    # Add the user
-    # ...
-    process_gsi(args)
-    return web.Response(text="JSON Received.")
-
-
-async def init_app() -> web.Application:
-    gsi = web.Application()
-    gsi.add_routes(routes)
-    return gsi
-
-
-web.run_app(init_app())
-
+if __name__ == '__main__':
+    server.start()
 
--- src/GameStateIntegration/gsi_server.js
+++ src/GameStateIntegration/gsi_server.js
@@ -1,6 +1,33 @@
-var d2gsi = require('dota2-gsi');
+var d2gsi = require("dota2-gsi");
 var server = new d2gsi();
-const axios = require('axios').default;
+const axios = require("axios").default;
+
+function sendGameState(gameStateObject) {
+  axios
+    .post("http://127.0.0.1:8080/json", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        Accept: "application/json",
+      },
+      // Stringify the payload into JSON:
+      body: JSON.stringify(gameStateObject),
+    })
+    .then((res) => {
+      console.log(res);
+      if (res.ok) {
+        return res.json();
+      } else {
+        console.log("something is wrong");
+      }
+    })
+    .then((jsonResponse) => {
+      // Log the response data in the console
+      console.log(jsonResponse);
+    })
+    .catch((err) => console.error(err));
+}
+
 /* Game started/ended.
      Heroes alive/dead.
      Friendly structure HPs.
@@ -12,46 +39,41 @@ const axios = require('axios').default;
          Heroes in match
          Fortification cooldown (?)
 */
-var cars = [{"make":"Porsche", "model":"911S"}]
-axios.post("http://127.0.0.1:8080/json",
- {
- method: 'POST',
- headers: {
- 'Content-type': 'application/json',
- 'Accept': 'application/json'
- },
-     // Strigify the payload into JSON:
- body:JSON.stringify(cars)}).then(res=>{
-          console.log(res)
- if(res.ok){
- return res.json()
- }else{
-    console.log("something is wrong")
- }
- }).then(jsonResponse=>{
 
- // Log the response data in the console
- console.log(jsonResponse)
- }
- ).catch((err) => console.error(err));
 
-server.events.on('newclient', function(client) {
-    console.log("New client connection, IP address: " + client.ip);
-    if (client.auth && client.auth.token) {
-        console.log("Auth token: " + client.auth.token);
-    } else {
-        console.log("No Auth token");
-    }
+server.events.on("newclient", function (client) {
 
-    client.on('player:activity', function(activity) {
-        if (activity == 'playing') console.log("Game started!");
-    });
-    client.on('hero:level', function(level) {
-        var a = JSON.stringify({ "name": name.value, "email": email.value });
-        console.log("Now level " + level);
+  console.log("New client connection, IP address: " + client.ip);
 
-    });
-    client.on('abilities:ability0:can_cast', function(can_cast) {
-        if (can_cast) console.log("Ability0 off cooldown!");
-    });
+  if (client.auth && client.auth.token) {
+    console.log("Auth token: " + client.auth.token);
+  } else {
+    console.log("No Auth token");
+  }
+
+  client.on("player:activity", function (activity) {
+    if (activity == "playing") console.log("Game started!");
+  });
+
+  client.on("", function (activity) {
+
+  });
+
+  client.on("hero:level", function (level) {
+    console.log(level);
+    console.log("Now level " + level);
+  });
+
+  client.on("abilities:ability0:can_cast", function (can_cast) {
+    if (can_cast) console.log("Ability0 off cooldown!");
+  });
+
+  client.on("hero:xpos", function (xpos) {
+    console.log("Hero x position: " + xpos)
+  })
+
+  client.on("hero:ypos", function (ypos) {
+    console.log("Hero y position: " + ypos)
+  })
+
 });
