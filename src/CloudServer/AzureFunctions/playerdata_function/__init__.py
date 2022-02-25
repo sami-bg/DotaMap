c0f721ec02bdc2802c4dd3b04f0efd39d2a2fec6
unknown <samibghanem@gmail.com>
2022-02-24 21:09:35 -0500

    azure fn skeleton

---
+++ src/CloudServer/AzureFunctions/playerdata_function/__init__.py
@@ -0,0 +1,21 @@
+import logging
+import azure.functions as func
+
+"""
+Player-data Function - Stores player data in redis, responds to clients asking for player-data 
+"""
+
+
+def main(request: func.HttpRequest):
+    logging.info(f'Request Method: {request.method}')
+
+    if request.method == 'GET':
+        return func.HttpResponse("Received GET")
+
+    # POST
+    try:
+        match_id = request.get_json()['match_id']
+    except (ValueError, KeyError):
+        return func.HttpResponse(f"Incorrect JSON: {request}")
+
+    return func.HttpResponse(f'Connected - Match ID: {match_id}')
